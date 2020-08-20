88e5a76387d9e881d43a72582e6c3bbf4b8ed397
kz7059 <kz7059@gmail.com>
2020-08-20 23:38:40 +0200

    képek

---
+++ js/login.js
@@ -0,0 +1,6 @@
+let container = document.querySelector(".container")
+Element.prototype.setUrgent = function () {
+    this.style.bgColor = "#CEDDE2"
+}
+Document.querySelector(".container").setUrgent()
+
