964372359e94517bb78dae2a3980ba0ff2542698
kz7059 <kz7059@gmail.com>
2020-08-31 21:56:32 +0200

    alakul

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
+/*
 let container = document.querySelector(".container")
 Element.prototype.setUrgent = function () {
     this.style.bgColor = "#CEDDE2"
 }
 Document.querySelector(".container").setUrgent()
+*/
+
 
