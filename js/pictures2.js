9155116ac74c37e1bdf8d141fac72d5330614f3b
Kiss Zoltán <kz7059@gmail.com>
2020-11-24 11:48:32 +0100

    p2.html - change

--- js/pictures2.js
+++ js/pictures2.js
@@ -1,7 +1,8 @@
+/*
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+ Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -18,3 +19,4 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+*/
